875b845eb197afe60124af3738bda0506bc47880
Louie-pixel <130101289+Louie-pixel@users.noreply.github.com>
2024-04-19 16:28:59 +0300

    Add files via upload

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,5 @@
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
-
-notify { 'Flask installation complete':
-  require => Package['Flask'],
+# Install puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
