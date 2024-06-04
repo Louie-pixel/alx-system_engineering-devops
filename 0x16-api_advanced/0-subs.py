e1a02bae865c7c6cc7748c65366a7e135ca7322c
Louie-pixel <130101289+Louie-pixel@users.noreply.github.com>
2024-06-04 20:17:27 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,28 @@
+#!/usr/bin/python3
+"""
+Using reddit's API
+"""
 import requests
+after = None
 
-def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'api_advanced-project'}
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            if 'data' in data and 'subscribers' in data['data']:
-                return data['data']['subscribers']
-        return 0
-    except requests.RequestException:
-        return 0
 
-if __name__ == "__main__":
-    import sys
+def recurse(subreddit, hot_list=[]):
+    """returning top ten post titles recursively"""
+    global after
+    user_agent = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'after': after}
+    results = requests.get(url, params=parameters, headers=user_agent,
+                           allow_redirects=False)
 
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+    if results.status_code == 200:
+        after_data = results.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        all_titles = results.json().get("data").get("children")
+        for title_ in all_titles:
+            hot_list.append(title_.get("data").get("title"))
+        return hot_list
     else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        return (None)
